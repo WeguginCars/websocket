8c4e2b7ec6da12de0e35e5a02debe3ea2ae784fe
nurmuhammad701 <nurmuhammadmel@gmail.com>
2025-06-04 10:41:25 +0500

    all

--- upload/minio.go
+++ upload/minio.go
@@ -14,21 +14,24 @@ import (
 
 type MinioUploader struct {
 	client *minio.Client
+	cfg    *config.Config
 }
 
 func NewMinioUploader() (*MinioUploader, error) {
 	fmt.Println("minioda")
 	cfg := config.Load()
+
 	client, err := minio.New(cfg.Minio.MINIO_ENDPOINT, &minio.Options{
 		Creds:  credentials.NewStaticV4(cfg.Minio.MINIO_ACCESS_KEY_ID, cfg.Minio.MINIO_SECRET_ACCESS_KEY, ""),
-		Secure: false,
+		Secure: false, // localhost uchun false
 	})
+
 	if err != nil {
 		return nil, fmt.Errorf("failed to create minio client: %v", err)
 	}
 
 	fmt.Println("chiqdi")
-	return &MinioUploader{client: client}, nil
+	return &MinioUploader{client: client, cfg: cfg}, nil
 }
 
 func (m *MinioUploader) UploadFile(bucketName string, file multipart.File, header *multipart.FileHeader) (string, error) {
@@ -43,36 +46,36 @@ func (m *MinioUploader) UploadFile(bucketName string, file multipart.File, heade
 	_, err := m.client.PutObject(ctx, bucketName, newFileName, file, header.Size, minio.PutObjectOptions{
 		ContentType: getContentType(fileExt),
 	})
+
 	if err != nil {
 		return "", fmt.Errorf("failed to upload file: %v", err)
 	}
 
 	// Set bucket policy for public access
 	policy := fmt.Sprintf(`{
-		"Version": "2012-10-17",
-		"Statement": [
-			{
-				"Effect": "Allow",
-				"Principal": {
-					"AWS": ["*"]
-				},
-				"Action": ["s3:GetObject"],
-				"Resource": ["arn:aws:s3:::%s/*"]
-			}
-		]
-	}`, bucketName)
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "AWS": ["*"]
+                },
+                "Action": ["s3:GetObject"],
+                "Resource": ["arn:aws:s3:::%s/*"]
+            }
+        ]
+    }`, bucketName)
 
 	err = m.client.SetBucketPolicy(ctx, bucketName, policy)
 	if err != nil {
 		return "", fmt.Errorf("failed to set bucket policy: %v", err)
 	}
 
-	// Generate URL
-	// UploadFile funktsiyasida:
-	url := fmt.Sprintf("http://%s/%s/%s", config.Load().Minio.MINIO_ENDPOINT, bucketName, newFileName)
+	// Generate public URL using MINIO_PUBLIC_URL
+	url := fmt.Sprintf("%s/%s/%s", m.cfg.Minio.MINIO_PUBLIC_URL, bucketName, newFileName)
+
 	fmt.Println("minioda2")
 	return url, nil
-
 }
 
 func getContentType(fileExt string) string {
